b12df7826e94738ed20380e950dd67118dd23ffc
Plastimytes <magezirichardelijah@gmail.com>
2024-10-31 09:53:50 +0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -2,6 +2,13 @@ function page(){
   return <div>
     <h1>What is an engine  ?</h1>
     <p>An engine is a machine that can convert some form of energy (obtained from a fuel) into useful mechanical power or motion. If the engine produces kinetic energy (energy of motion) from a fuel source, it is called a prime mover; if it produces kinetic energy from a preprocessed "fuel" (such as electricity, a flow of hydraulic fluid, or compressed air), it is called a motor.</p>
+    <p>The story of the 2JZ-GTE begins in the late 1980s and early 1990s, a time when Toyota was looking to enhance its presence in the high-performance vehicle market. 
+
+At that point, Toyota’s flagship performance straight six was the three-litre 7M-GTE, which powered the third-gen Supra (A70) and second-gen Soarer (Z20). While it was a decent engine for its time, it did suffer from several well-known weaknesses and lacked the robustness Toyota was after.
+
+The goal was to develop an engine that could rival the best in the industry, particularly the game-changing RB26DETT powering the recently-released Nissan R32 Skyline GT-R. Toyota aimed to create a powerplant that was not only powerful but also exceptionally reliable and capable of handling far greater power levels in motorsport applications without compromising durability.
+
+The JZ family of engines debuted in 1990 with the introduction of the 1JZ-GTE, a 2.5-litre inline-six twin-turbo engine. The success of the 1JZ-GTE laid the groundwork for its larger and more powerful sibling. </p>
   </div>
 }
 export default page;
